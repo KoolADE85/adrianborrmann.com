24b086cf65cf4df20d7ecb69cd00d84983a35b41
Adrian Borrmann <hello@adrianborrmann.com>
2019-09-03 20:09:29 -0400

    Updates

--- adrianborrmann/adrianborrmann/static/dist/adrianborrmann.initialize.js
+++ adrianborrmann/adrianborrmann/static/dist/adrianborrmann.initialize.js
@@ -1,3 +1,3 @@
-/*! Adrian Borrmann Portfolio Website - http://adrianborrmann.com last modified 2017-10-07*/
+/*! Adrian Borrmann Portfolio Website - http://adrianborrmann.com last modified 2019-09-03*/
 (function(){var a=this,b=a.jQuery,c=a.AdrianBorrmann||(a.AdrianBorrmann={}),d=c.Core=function(a){var c={};this.config=b.extend(!0,c,a||{}),this._initializePage()};d.prototype._initializePage=function(){b("html").removeClass("loading");var a=b("body").prop("class").split(" "),c=a.length;if(c>0)for(;c--;)switch(a[c]){case"resume-page":this._initResume();break;case"home":}},d.prototype._initResume=function(){b("section").on("click",function(){b("section.active").removeClass("active"),b(this).addClass("active")})}}).call(this);
 //# sourceMappingURL=adrianborrmann.initialize.js.map
