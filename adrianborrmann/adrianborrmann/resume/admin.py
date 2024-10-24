58bbc4404987f58eddf535a1b234ff4f69892ae0
Adrian Borrmann <hello@adrianborrmann.com>
2024-10-24 17:00:27 -0600

    empty

--- adrianborrmann/adrianborrmann/resume/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from adrianborrmann.resume.models import Website
-
-admin.site.register(Website)
--- adrianborrmann/adrianborrmann/resume/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-
-urlpatterns = patterns('adrianborrmann.resume.views',
-    url(r'^$', 'home', name='resume'),
-)
